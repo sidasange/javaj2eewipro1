d2bbbcfafd3bbaf2d3c252c60945310216ce7643
sidasange <7356kamsi@gmail.com>
2024-03-12 15:04:25 +0530

    first commit

---
+++ corejava/DataTypesDemo.java
@@ -0,0 +1,25 @@
+class DataTypesDemo{
+ public static void main(String[] args){
+  
+byte b = 10;
+
+int i = 500;
+
+float f = 20.5f;
+
+double d =10.5;
+
+char ch ='z';
+
+boolean g= true;
+
+System.out.println("byte ="+b);
+System.out.println("integer ="+i);
+System.out.println("double ="+d);
+System.out.println("float ="+f);
+System.out.println("char ="+ch);
+System.out.println("boolean ="+g);
+
+
+ }
+}
